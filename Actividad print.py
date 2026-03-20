e501fd7888997878ab3bd5dae66b4bcfa4c05d4a
mjimenez157 <mjimenez157@unab.edu.co>
2026-03-19 21:13:45 -0500

    Actividad print.py

--- Actividad print.py
+++ Actividad print.py
@@ -7,5 +7,7 @@ print(" ****   **** ")
 print("   **   **   ")
 print("   **   **   ")
 print("   *******   ")
+#
 print(duplicar)
+# Si cambio print por Print va a salir NameError: name 'print' is not defined
 print(duplicar * 2)
