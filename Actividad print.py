b8b6409516bb700a75ae48083e92afad68adabe2
mjimenez157 <mjimenez157@unab.edu.co>
2026-03-19 21:16:25 -0500

    Actividad print.py

--- Actividad print.py
+++ Actividad print.py
@@ -7,7 +7,7 @@ print(" ****   **** ")
 print("   **   **   ")
 print("   **   **   ")
 print("   *******   ")
-#
+# Si se usa '' en vez de "" es aceptable mientras no se ponga mezcladas '"
 print(duplicar)
 # Si cambio print por Print va a salir NameError: name 'print' is not defined
 print(duplicar * 2)
